c4b439c121995c97d414143b2f34cad0436ca580
AnnaRossova <rossova.anna@gmail.com>
2024-06-23 17:08:50 +0300

    updated

--- src/server.js
+++ src/server.js
@@ -34,37 +34,3 @@ const startServer = async () => {
 
 export default startServer;
 
-/* import express from 'express';
-import cors from 'cors';
-import pino from 'pino-http';
-import dotenv from 'dotenv';
-import contactsRouter from './routes/contacts.js';
-
-dotenv.config();
-
-const setupServer = () => {
-  const app = express();
-  const port = process.env.PORT || 3001;
-
-  app.use(cors());
-  app.use(pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }));
-  app.use(express.json());
-
-  app.use('/contacts', contactsRouter);
-
-  app.use((req, res) => {
-    res.status(404).json({ status: 404, message: 'Not found' });
-  });
-
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-};
-
-export default setupServer;
-
- */
